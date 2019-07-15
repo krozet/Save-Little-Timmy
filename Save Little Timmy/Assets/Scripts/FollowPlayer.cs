c98d04f0ad917806ab3685ef23d1e1f3d8b26cf0
krozet <keawa.rozet@yahoo.com>
2019-07-15 13:53:49 -0700

    slerp camera movement added.

---
+++ Save Little Timmy/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform CrazyJoe;
+    private Vector3 cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - CrazyJoe.position;
+    }
+
+    // LateUpdate is called after Update
+    void LateUpdate()
+    {
+        Vector3 newPos = CrazyJoe.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+    }
+}
