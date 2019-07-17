97048ad13337eac72b2bd1e8d3141d5af9f2a430
dfraats <derek.fraats@yahoo.com>
2019-07-16 17:24:11 -0700

    Added water bottle asset and code

---
+++ Save Little Timmy/Assets/Scripts/WaterBottle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBottle : MonoBehaviour {
+
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("CrazyJoe"))
+        {
+            Pickup();
+        }   
+    }
+
+    void Pickup()
+    {
+        // Spawn an effect
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        // Fill up water Gauge
+
+        // Remove WaterBottle
+        Destroy(gameObject);
+    }
+}
