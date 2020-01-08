b25ba758faf46640c3790f2042d37b380dc0245e
Keawa <keawa.rozet@yahoo.com>
2020-01-08 14:03:28 -0800

    camera now snaps to a fixed distance from crazy joe.

--- Save Little Timmy/Assets/Scripts/Crazy Joe/FollowPlayer.cs
+++ Save Little Timmy/Assets/Scripts/Crazy Joe/FollowPlayer.cs
@@ -7,19 +7,17 @@ public class FollowPlayer : MonoBehaviour
 
     public Transform crazyJoe;
     private Vector3 cameraOffset;
-    //private Vector3 startingCameraOffset;
+    private Vector3 startingCameraOffset;
 
     [Range(0.01f, 1.0f)]
     public float SmoothFactor = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        //startingCameraOffset = new Vector3(4.0f, 8.6f, -5.7f);
-        //transform.position = crazyJoe.position;
-        //transform.Translate(startingCameraOffset.normalized * 1);
+        startingCameraOffset = new Vector3(4.0f, 8.6f, -5.7f);
+        transform.position = crazyJoe.position + startingCameraOffset;
         
         cameraOffset = transform.position - crazyJoe.position;
-        //Debug.Log("OFFSET " + cameraOffset.);
     }
 
     // LateUpdate is called after Update
