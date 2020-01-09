f71a5588b6e0b7e2e36fb13e00a3e6fe7451b5b2
Keawa <keawa.rozet@yahoo.com>
2020-01-08 16:00:12 -0800

    started template for auido manager.

--- Save Little Timmy/Assets/Scripts/AudioManager.cs
+++ Save Little Timmy/Assets/Scripts/AudioManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    // Insert audio instance variables here
+
     // Start is called before the first frame update
     void Start()
     {
