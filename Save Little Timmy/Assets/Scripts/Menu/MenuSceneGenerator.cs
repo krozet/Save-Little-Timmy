35723b40a7d41b9a09e4b1f562d7496ef042b0c9
krozet <keawa.rozet@yahoo.com>
2020-01-14 15:21:44 -0800

    created barebones version.

---
+++ Save Little Timmy/Assets/Scripts/Menu/MenuSceneGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSceneGenerator : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    private GameObject buildingSpawnPoint;
+    private List<GameObject> instanciatedObjects;
+    private float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instanciatedObjects = new List<GameObject>();
+        buildingSpawnPoint = GameObject.Find("BuildingSpawnPoint");
+        RandomInstanciation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*foreach (GameObject obj in instanciatedObjects) {
+            obj.GetComponent<Rigidbody>().velocity = obj.transform.TransformDirection(Vector3.forward * speed);
+        }*/
+    }
+
+    public void RandomInstanciation() {
+        int randomIndex = (int)Random.Range(0, objects.Length - 1f);
+        GameObject temp = Instantiate(objects[0],
+                                            buildingSpawnPoint.transform.position,
+                                            Quaternion.identity);
+        temp.GetComponent<Rigidbody>().velocity = Vector3.forward * speed;
+        instanciatedObjects.Add(temp);
+    }
+}
