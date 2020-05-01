ce5650b3b49470795a82c776f9a4111d79c2c354
krozet <keawa.rozet@yahoo.com>
2020-05-01 15:54:09 -0700

    janky fps controller.

--- Save Little Timmy/Assets/Scripts/Crazy Joe/FollowPlayer.cs
+++ Save Little Timmy/Assets/Scripts/Crazy Joe/FollowPlayer.cs
@@ -11,7 +11,7 @@ public class FollowPlayer : MonoBehaviour
 
     [Header("Speed which Camera follows Crazy Joe")]
     [Range(0.01f, 1.0f)]
-    public float SmoothFactor = 0.5f;
+    public float SmoothFactor = 0f;
     [Header("Distance from Crazy Joe")]
     public Vector3 AdjustableCameraOffset;
     [Header("Rotation")]
@@ -22,20 +22,21 @@ public class FollowPlayer : MonoBehaviour
     void Start()
     {
         AdjustableCameraOffset = Vector3.zero;
-        startingCameraOffset = new Vector3(4.0f, 8.6f, -5.7f);
+        //startingCameraOffset = new Vector3(4.0f, 8.6f, -5.7f);
+        startingCameraOffset = new Vector3(0f, 1.8f, 0f);
         transform.position = crazyJoe.position + startingCameraOffset;
         
         cameraOffset = transform.position - crazyJoe.position;
-
-        rotation = transform.rotation;
+        transform.rotation = crazyJoe.rotation;
     }
 
     // LateUpdate is called after Update
     void LateUpdate()
     {
         Vector3 newPos = crazyJoe.position + cameraOffset + AdjustableCameraOffset;
+        rotation = new Quaternion(transform.rotation.x, crazyJoe.rotation.y, transform.rotation.z, transform.rotation.w);
 
         transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.8f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, SmoothFactor);
     }
 }
