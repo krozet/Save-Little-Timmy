abc0785e0e729f8b71a6f7a57d913f61bf88c95f
krozet <keawa.rozet@yahoo.com>
2020-05-20 14:56:12 -0700

    added new method of issuing animations using degrees.

---
+++ Save Little Timmy/Assets/Scripts/Crazy Joe/AnimationValue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimationValue
+{
+    // Player movement and look direction animation point values
+    // animationMovementDirection, animationLookDirection
+    public static int IDLE = 0;
+    public static int W, deg0 = 1;
+    public static int WD, deg45 = 2;
+    public static int D, deg90 = 3;
+    public static int SD, deg135 = 4;
+    // 180 and -180
+    public static int S, deg180 = 5;
+    public static int AS, deg225 = 6;
+    public static int A, deg270 = 7;
+    public static int AW, deg315 = 8;
+
+    // Player movement animation
+    public static int RUN_FORWARD = 0;
+    public static int FORWARD_STRAFE_RIGHT = 1;
+    public static int STRAFE_RIGHT = 2;
+    public static int BACKWARD_STRAFE_RIGHT = 3;
+    public static int BACKWARD = 4;
+    public static int BACKWARD_STRAFE_LEFT = 5;
+    public static int STRAFE_LEFT = 6;
+    public static int FORWARD_STRAFE_LEFT = 7;
+}
--- Save Little Timmy/Assets/Scripts/Crazy Joe/PlayerController.cs
+++ Save Little Timmy/Assets/Scripts/Crazy Joe/PlayerController.cs
@@ -21,6 +21,9 @@ public class PlayerController : MonoBehaviour
     private Transform penis;
     private Animator animator;
 
+    public int currentLookDirection = AnimationValue.deg0;
+    public int currentMovementDirection = AnimationValue.W;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +31,12 @@ public class PlayerController : MonoBehaviour
         crazyJoe = GetComponent<CrazyJoe>();
         penis = crazyJoe.GetComponentInChildren<Penis>().transform;
         animator = GetComponentInChildren<Animator>();
+
+        SetCurrentLookDirection();
+    }
+
+    void runTestValues(int x, int y) {
+        Debug.Log("Ran these values, movement = " + x + " and look = " + y + " gets a value of = " + GetMovementAnimationValue(x,y));
     }
 
     // Update is called once per frame
@@ -43,6 +52,14 @@ public class PlayerController : MonoBehaviour
 
         //Player rotation towards mouse
         SetRotationTowardsMouse();
+
+        SetCurrentLookDirection();
+
+        SetCurrentMovementDirection();
+
+        SetAnimationBlendValue();
+
+
 
         // Left Click to piss
         if (Input.GetMouseButton(0)) {
@@ -90,42 +107,66 @@ public class PlayerController : MonoBehaviour
         currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
 
         transform.position += desiredMoveDirection * currentSpeed * Time.deltaTime;
-        SetDirectionalAnimations();
+
         animator.SetFloat("velocity", currentSpeed);
     }
 
-    private void SetDirectionalAnimations() {
+    private void SetCurrentMovementDirection() {
         switch(Input.GetAxisRaw("Horizontal")) {
+            //left
             case -1:
-                if (!animator.GetBool("right")) {
-                    animator.SetBool("left", true);
+                // up/left
+                if (currentMovementDirection == AnimationValue.W ) {
+                    currentMovementDirection = AnimationValue.AW;
+                // down/left
+                } else if (currentMovementDirection == AnimationValue.S) {
+                    currentMovementDirection = AnimationValue.AS;
+                } else {
+                    currentMovementDirection = AnimationValue.A;
                 }
                 break;
+            // right
             case 1:
-                if (!animator.GetBool("left")) {
-                    animator.SetBool("right", true);
+                // up/right
+                if (currentMovementDirection == AnimationValue.W) {
+                    currentMovementDirection = AnimationValue.WD;
+                    // down/right
+                } else if (currentMovementDirection == AnimationValue.S) {
+                    currentMovementDirection = AnimationValue.SD;
+                } else {
+                    currentMovementDirection = AnimationValue.D;
                 }
                 break;
             default:
-                animator.SetBool("right", false);
-                animator.SetBool("left", false);
                 break;
         }
 
         switch (Input.GetAxisRaw("Vertical")) {
+            // down
             case -1:
-                if (!animator.GetBool("forward")) {
-                    animator.SetBool("backward", true);
+                // down/left
+                if (currentMovementDirection == AnimationValue.A) {
+                    currentMovementDirection = AnimationValue.AS;
+                    // down/right
+                } else if (currentMovementDirection == AnimationValue.D) {
+                    currentMovementDirection = AnimationValue.SD;
+                } else {
+                    currentMovementDirection = AnimationValue.S;
                 }
                 break;
+            // up
             case 1:
-                if (!animator.GetBool("backward")) {
-                    animator.SetBool("forward", true);
+                // up/left
+                if (currentMovementDirection == AnimationValue.A) {
+                    currentMovementDirection = AnimationValue.AW;
+                    // up/right
+                } else if (currentMovementDirection == AnimationValue.D) {
+                    currentMovementDirection = AnimationValue.WD;
+                } else {
+                    currentMovementDirection = AnimationValue.W;
                 }
                 break;
             default:
-                animator.SetBool("backward", false);
-                animator.SetBool("forward", false);
                 break;
         }
     }
@@ -151,7 +192,11 @@ public class PlayerController : MonoBehaviour
         if (crazyJoePlace.Raycast(cameraRay, out rayLength)) {
             mousePosition = cameraRay.GetPoint(rayLength);
             direction = new Vector3(mousePosition.x, transform.position.y, mousePosition.z);
+            Quaternion lookDirection = Quaternion.LookRotation((direction - transform.position), Vector3.up);
+            Vector3 cross = Vector3.Cross(transform.rotation * Vector3.forward, lookDirection * Vector3.forward);
+            //Debug.Log("Look direction = " + cross);
             transform.LookAt(direction);
+            //Debug.Log("Rotation = " + transform.rotation.eulerAngles);
             penis.localEulerAngles = transform.localEulerAngles;
         }
 
@@ -159,6 +204,62 @@ public class PlayerController : MonoBehaviour
         if (debug) {
             Debug.DrawLine(cameraRay.origin, mousePosition, Color.red);
         }
+    }
+
+    private void SetCurrentLookDirection() {
+        float degree = transform.rotation.eulerAngles.y;
+        float midDegree = 22.5f;
+
+        if ((degree >= 0 && degree < 0 + midDegree) || (degree >= 360 - midDegree && degree <=360)) {
+            currentLookDirection = AnimationValue.deg0;
+        } else if (degree >= 45 - midDegree && degree < 90 - midDegree) {
+            currentLookDirection = AnimationValue.deg45;
+        } else if (degree >= 90 - midDegree && degree < 135 - midDegree) {
+            currentLookDirection = AnimationValue.deg90;
+        } else if (degree >= 135 - midDegree && degree < 180 - midDegree) {
+            currentLookDirection = AnimationValue.deg135;
+        } else if (degree >= 180 - midDegree && degree < 225 - midDegree) {
+            currentLookDirection = AnimationValue.deg180;
+        } else if (degree >= 225 - midDegree && degree < 270 - midDegree) {
+            currentLookDirection = AnimationValue.deg225;
+        } else if (degree >= 270 - midDegree && degree < 315 - midDegree) {
+            currentLookDirection = AnimationValue.deg270;
+        } else {
+            currentLookDirection = AnimationValue.deg315;
+        }
+    }
+
+    private void SetAnimationBlendValue() {
+        int movementAnimationValue = GetMovementAnimationValue(currentMovementDirection, currentLookDirection);
+        float degree = transform.rotation.eulerAngles.y;
+        // use this to properly offset the look direction from the movement direction
+        float offset = movementAnimationValue * 45f;
+        float adjustedDegree = degree + offset;
+        // if the degree is now negative, readjust to fit 0-360 scale
+        if (adjustedDegree < 0) {
+            adjustedDegree += 360;
+        }
+
+        if (adjustedDegree > 360) {
+            adjustedDegree -= 360;
+        }
+
+        animator.SetFloat("degrees", adjustedDegree);
+
+        Debug.Log("degree = " + degree +
+            " offset = " + offset +
+            " adjustedDegree = " + adjustedDegree);
+    }
+
+    public int GetMovementAnimationValue(int animationMovementDirection, int animationLookDirection) {
+        int distance = 0;
+        for (int i = animationMovementDirection; i != animationLookDirection; i++) {
+            if (i % 8 == 0) {
+                i = 0;
+            }
+            distance++;
+        }
+        return distance;
     }
 
     public Vector3 GetLookAtDirection() {
