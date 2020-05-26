1d592b4951b0f2f76e5109ec4d6b71eb103b1607
krozet <keawa.rozet@yahoo.com>
2020-05-26 14:53:35 -0700

    created LogMaster class.

---
+++ Save Little Timmy/Assets/Scripts/LogMaster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogMaster
+{
+    string longLog = "";
+    int numberOfLines = 3;
+
+    public LogMaster() {
+
+    }
+
+    public void Append<T>(string log, T value) {
+        longLog += log + ": " + value.ToString() + "\n";
+        numberOfLines++;
+    }
+
+    public void Append(string log) {
+        longLog += log + "\n";
+        numberOfLines++;
+    }
+
+    public void PrintLongLog() {
+        Debug.Log("-------------------------\n" + 
+            "Total number of Lines: " + numberOfLines + "\n" +
+            longLog +
+            "-------------------------\n");
+    }
+
+    public void Clear() {
+        longLog = "";
+        numberOfLines = 3;
+    }
+
+    public static void QuickLog<T>(string log, T value) {
+        Debug.Log(log + ": " + value);
+    }
+}
+
+
